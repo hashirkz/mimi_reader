e5e80e8a422982be767afb586a69029a3c5e73c1
hashir <75959275+zzsleepyzzz@users.noreply.github.com>
2022-06-23 03:35:56 -0400

    updates

--- manga_webscrapers/manganelo_scrapper.js
+++ manga_webscrapers/manganelo_scrapper.js
@@ -1,14 +1,14 @@
 // fetch script for manganelo *readmanganato.com* searches
 
-import fetch from 'node-fetch';
+const fetch = require('fetch');
+const puppeteer = require('puppeteer');
 
-let read_data = function(path){
-    __text = '';
+let read_data = async path => {
 
-    fetch(path)
-        .then(__text.append(response));
+    const __text = await fetch(path);
+    const __json = await __text.json();
     
-    console.log(__text);
+    console.log(__json);
 }
 
 console.log(read_data('https://readmanganato.com/'));
